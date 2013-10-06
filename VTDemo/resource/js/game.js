46b76daf38970a6a6e36c625855a1ebc653de0b0
Sam <notmye@gmail.com>
2013-10-06 14:27:24 +0100

    Update

--- VTDemo/resource/js/game.js
+++ VTDemo/resource/js/game.js
@@ -1,6 +1,7 @@
 //Game files world cell functions etc
+var DEBUG = false;
 
-function Group(X,Y)// one part of the person
+function Group(x,y)// one part of the person
 {
 	this.X = x;
 	this.Y = y;
@@ -9,9 +10,12 @@ function World()
 {
 	this.CellCount = 50;
 	this.Cells = new Array(50);
-	for(var i = 0;i< this.CellCount)
+	for(var i = 0;i< this.CellCount;++i)
 	{
 		this.Cells = new Group();
 	}
-	
-};
+	if(DEBUG){console.log("built group");}
+	this.Update = function()
+	{
+	};
+}
