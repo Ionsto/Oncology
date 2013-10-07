4e669f08ecd0939a6b26c1f5bc56abcbfe465ead
Sam <notmye@gmail.com>
2013-10-07 21:42:18 +0100

    update

--- VTDemo/resource/js/game.js
+++ VTDemo/resource/js/game.js
@@ -14,9 +14,27 @@ function Group(x,y)// one part of the person
 	this.Rad = 0;
 	this.Type = 0;
 }
+function RadEmitter(x,y)
+{
+	this.SX = x;//mid point of circle
+	this.SY = y;
+	this.Rad = 10;
+	this.X = Rad;
+	this.Y = 0;
+	this.Angle = 0;
+	this.UpdatePos = function(Mx,My)
+	{
+		var theta = Math.atan2f(My - this.SY,Mx - this.SX);
+		this.X = Rad * Math.cos(theta);
+		this.Y = Rad * Math.sin(theta);
+		return;
+	};
+}
 function World(Xw,Yw)
 {
 	this.ColourArr = ["#FFFFFF","#FFFFAA","#FFFF00","#FF0000","#00FF00","#0000FF"];
+	this.Rads = new Array(3);
+	this.Rads[0] = new RadEmitter(Xw/2,Yw/2);
 	this.SizeX = Xw;
 	this.SizeY = Yw;
 	this.Clear = true;
