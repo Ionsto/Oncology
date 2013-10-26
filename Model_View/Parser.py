1cfb8d8a957d8a2dc9633c151db4b518bdba1186
sam <am.sutcliffe2@gmail.com>
2013-10-26 11:09:21 +0100

    Update

--- Model_View/Parser.py
+++ Model_View/Parser.py
@@ -1,8 +1,12 @@
-f = open('workfile.txt', 'r')
+f = open('C:/Users/mpfs/Contacts/Documents/GitHub/Oncology/Model_View/workfile.txt', 'r')
 while True:
 	strs = f.readline()
 	if(strs == ""):
 		break
 	else:
-		print(strs)
+		strsS = strs.split(",")
+		if(strsS[0] == "Herro"):
+			int(print(strsS[1]))
+			int(print(strsS[2]))
+			int(print(strsS[3]))
 f.close()
