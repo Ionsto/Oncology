320e6d58eab0aa09b05622b514b77dd391f2cd2f
sam <am.sutcliffe2@gmail.com>
2013-10-27 11:20:29 +0000

    updatyer

--- Model_View/Parser.py
+++ Model_View/Parser.py
@@ -1,3 +1,4 @@
+import bpy
 f = open('C:/Users/mpfs/Contacts/Documents/GitHub/Oncology/Model_View/workfile.txt', 'r')
 while True:
 	strs = f.readline()
@@ -5,8 +6,10 @@ while True:
 		break
 	else:
 		strsS = strs.split(",")
-		if(strsS[0] == "Herro"):
-			int(print(strsS[1]))
-			int(print(strsS[2]))
-			int(print(strsS[3]))
+		if(strsS[0] == "slice"):
+			me = bpy.data.meshes.new("mesh")
+			ob = bpy.data.objects.new("slice", me)
+			ob.location.x = int(strsS[1])
+			ob.location.y = int(strsS[2])
+			ob.location.z = int(strsS[3])
 f.close()
