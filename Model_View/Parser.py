c78fae7f5a3f75519ea8e0549adbf2a05d26d69e
Sam <notmye@gmail.com>
2013-11-09 12:36:30 +0000

    Update

--- Model_View/Parser.py
+++ Model_View/Parser.py
@@ -9,7 +9,12 @@ while True:
 		if(strsS[0] == "slice"):
 			me = bpy.data.meshes.new("mesh")
 			ob = bpy.data.objects.new("slice", me)
-			ob.location.x = int(strsS[1])
-			ob.location.y = int(strsS[2])
-			ob.location.z = int(strsS[3])
+			if strsS[1] == "loc":
+				ob.location.x = int(strsS[2])
+				ob.location.y = int(strsS[3])
+				ob.location.z = int(strsS[4])
+			if strsS[1] == "sca":
+				ob.scale.x = int(strsS[2])
+				ob.scale.y = int(strsS[3])
+				ob.scale.z = int(strsS[4])
 f.close()
